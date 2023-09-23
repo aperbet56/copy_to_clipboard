c68a178d287bf8b75b9a7765f74163256cba994d
Annabelle Perbet <annabelleperbet@yahoo.fr>
2023-09-23 16:12:05 +0200

    Ajout de commentaires

--- script.js
+++ script.js
@@ -9,10 +9,14 @@ document.querySelectorAll(".copy__link").forEach((copyLinkParent) => {
 
   // Ecoute de l'événement "click" sur le bouton copier
   copyBtn.addEventListener("click", () => {
+    // La méthode select() sélectionne tout le texte d'un élément <textarea> ou d'un élément <input> qui inclut un champ de texte.
     inputField.select();
+
+    // La méthode writeText() de l'interface Clipboard écrit le texte spécifié dans le presse-papiers du système
     navigator.clipboard.writeText(text);
 
     inputField.value = "Copied!";
+    // La méthode globale setTimeout() permet de définir un minuteur qui exécute une fonction ou un code donné après la fin du délai indiqué.
     setTimeout(() => (inputField.value = text), 2500);
   });
 });
